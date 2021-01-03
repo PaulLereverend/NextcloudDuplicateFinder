97d78147a7032b1bb58caefad4e8b213a5025964
Sandesh Shrestha <mail@sandeshshrestha.com>
2021-01-03 13:50:07 +0100

    Refactor
    Refactored the code only, functionality is same as before

--- js/script.js
+++ js/script.js
@@ -1,150 +1,204 @@
-var total_size = 0;
-var nb_element = 0;
+(function() {
+  var baseUrl = OC.generateUrl('/apps/duplicatefinder');
+  var element = document.getElementById('container');
+  var loader = document.getElementById('loader-container');
+  var title = document.getElementById('title');
+  var items = [];
 
-loadList();
+  function render() {
+    loader.style.display = 'none';
 
-function loadList() {
-    var baseUrl = OC.generateUrl('/apps/duplicatefinder');
-    var element = document.getElementById('container');
-    $.getJSON(baseUrl + '/files')
-        .then(function (result) {
-            let previous_hash = '';
+    if (items.length == 0) {
+      updateTitle("0 duplicate file found");
+    } else {
+      updateTitleWithStats();
 
-            result.sort((b, a) => parseFloat(a.infos.size) - parseFloat(b.infos.size));
-            result.forEach(el => {
-                var div = document.createElement("div");
-                div.setAttribute('id', el.infos.id);
-                div.setAttribute('class', 'element')
-                var img = document.createElement("div");
+      // Sort desending by size
+      items.sort((a, b) => b.infos.size - a.infos.size);
 
-                var button = document.createElement("button");
-                button.innerHTML = '<span class="icon icon-delete"></span>';
-                button.setAttribute('class', 'button-delete');
-                button.addEventListener("click", function () {
-                    deleteElement(el.path, el.infos.id, el.infos.size);
-                });
+      // Group items with same hash.
+      var groupedResult = groupBy(items, function(item) {
+        return item.hash;
+      });
 
-                div.appendChild(button);
+      groupedResult.hashes.forEach(function(hash) {
+        var groupItems = groupedResult.groupedItems[hash];
+        var gruopDOM = getGroupElement(groupItems);
+        element.appendChild(gruopDOM);
+      });
+    }
+  }
 
-                var label = document.createElement("div");
-                var path = document.createElement("h1");
-                path.setAttribute('class', 'path');
-                path.innerHTML = el.path;
-                label.appendChild(path);
+  function updateTitle(s_title) {
+    title.innerHTML = s_title;
+  }
 
-                function get_icons(actions, el) {
-                    var icons = document.createElement("div");
-                    icons.setAttribute('class', 'fileactions');
+  function updateTitleWithStats() {
+    var stats = getStats();
 
-                    actions.forEach(icon_info => {
-                        var icon_link = document.createElement('a');
-                        icon_link.setAttribute('href', icon_info.url.call(null, el));
-                        icon_link.setAttribute('class', 'action permanent');
-                        icon_link.setAttribute('title', icon_info.description)
+    updateTitle(stats.count + ' files found. Total: ' + OC.Util.humanFileSize(stats.totalSize))
+  }
 
-                        var action_icon = document.createElement('span');
-                        action_icon.setAttribute('class', 'icon ' + icon_info.icon);
-                        icon_link.appendChild(action_icon);
+  function getStats() {
+    return {
+      count: items.length,
+      totalSize: items.map(item => item.infos.size).reduce((a, b) => a + b, 0)
+    }
+  }
 
-                        // var action_text = document.createElement('span');
-                        // action_text.innerText = icon_info.icon;
-                        // icon_link.appendChild(action_text);
+  function isImage(item) {
+    return item.infos.mimetype.substr(0, item.infos.mimetype.indexOf('/')) == 'image';
+  }
 
-                        icons.appendChild(icon_link);
-                    });
+  function isVideo(item) {
+    return item.infos.mimetype.substr(0, item.infos.mimetype.indexOf('/')) == 'video';
+  }
 
-                    return icons;
-                }
+  function getPreviewImage(item) {
+    if (isImage(item) || isVideo(item)) {
+      return OC.generateUrl('/core/preview.png?') + $.param({
+        file: item.path,
+        fileId: item.infos.id,
+        x: 500,
+        y: 500,
+        forceIcon: 0
+      });
+    }
 
-                // Get icons
-                var actions = [{
-                    'icon': 'icon-file',
-                    'url': function (el) {
-                        let path = el.path;
-                        let dir = OC.dirname(path);
-                        return OC.generateUrl('/apps/files/?dir=' + dir + '&openfile=' + el.infos.id);
-                    },
-                    'description': 'Show file'
-                }, {
-                    'icon': 'icon-details',
-                    'url': function (el) {
-                        return OC.generateUrl('/f/' + el.infos.id);
-                    },
-                    'description': 'Show details'
-                }];
-                var icons = get_icons(actions, el);
-                label.appendChild(icons);
+    return OC.MimeType.getIconUrl(item.infos.mimetype);
+  }
 
-                var hash = document.createElement("h1");
-                hash.setAttribute('class', 'hash');
-                hash.innerHTML = el.hash;
-                label.appendChild(hash);
 
-                img.setAttribute('class', 'thumbnail');
-                if (el.infos.mimetype.substr(0, el.infos.mimetype.indexOf('/')) == 'image' ||
-                    el.infos.mimetype.substr(0, el.infos.mimetype.indexOf('/')) == 'video') {
-                    var params = {
-                        file: el.path,
-                        fileId: el.infos.id,
-                        x: 500,
-                        y: 500,
-                        forceIcon: 0
-                    };
-
-                    const previewUrl = OC.generateUrl('/core/preview.png?') + $.param(params);
-                    img.style.backgroundImage = "url('" + previewUrl + "')";
-                } else {
-                    const iconUrl = OC.MimeType.getIconUrl(el.infos.mimetype);
-                    img.style.backgroundImage = "url(" + iconUrl + ")";
-                }
-
-                div.appendChild(img);
-                div.appendChild(label);
-
-                if (el.hash == previous_hash) {
-                    group.appendChild(div);
-                } else {
-                    if (typeof group != "undefined") {
-                        element.appendChild(group);
-                    }
-                    group = getGroupeDiv();
-                    // Show size
-                    size = document.createElement("div");
-                    size.setAttribute('class', 'filesize');
-                    size.innerHTML = OC.Util.humanFileSize(el.infos.size);
-                    group.prepend(size);
-                    group.appendChild(div);
-                }
-                previous_hash = el.hash;
-                total_size += el.infos.size;
-
-            });
-            document.getElementById('loader-container').style.display = 'none';
-            if (result.length == 0) {
-                document.getElementById('title').innerHTML = "0 duplicate file found";
-            } else {
-                nb_element = result.length;
-                updateTitle();
-            }
-            element.appendChild(group);
-        });
-}
-
-function deleteElement(path, id, size) {
+  function deleteItem(item) {
     let fileClient = OC.Files.getClient();
-    fileClient.remove(path);
-    document.getElementById(id).remove();
-    total_size -= size;
-    nb_element--;
-    updateTitle();
-}
+    fileClient.remove(item.path);
+    document.getElementById(item.infos.id).remove();
+    var itemIndex = items.findIndex(function(i) {
+      return i.infos.id === item.infos.id;
+    });
+    items.splice(itemIndex, 1);
+    updateTitleWithStats();
+  }
 
-function getGroupeDiv() {
-    let div = document.createElement("div");
-    div.setAttribute('class', 'duplicates');
-    return div;
-}
 
-function updateTitle() {
-    document.getElementById('title').innerHTML = nb_element + ' files found. Total: ' + OC.Util.humanFileSize(total_size);
-}
+  function getGroupElement(groupItems) {
+    let groupContainer = document.createElement("div");
+    groupContainer.setAttribute('class', 'duplicates');
+
+    var sizeDiv = document.createElement("div");
+    sizeDiv.setAttribute('class', 'filesize');
+    sizeDiv.innerHTML = OC.Util.humanFileSize(groupItems[0].infos.size);
+    groupContainer.appendChild(sizeDiv);
+
+    groupItems.forEach(function(item) {
+      var itemDiv = getItemElement(item);
+      groupContainer.appendChild(itemDiv);
+    });
+
+    return groupContainer;
+  }
+
+
+  function getItemElement(item) {
+    // Item wrapper container
+    var itemDiv = document.createElement("div");
+    itemDiv.setAttribute('id', item.infos.id);
+    itemDiv.setAttribute('class', 'element')
+
+    // Delete button
+    var deleteButton = document.createElement("button");
+    deleteButton.innerHTML = '<span class="icon icon-delete"></span>';
+    deleteButton.setAttribute('class', 'button-delete');
+    deleteButton.addEventListener("click", function() {
+      deleteItem(item);
+    });
+    itemDiv.appendChild(deleteButton);
+
+    // Preview image
+    var previewImage = document.createElement("div");
+    previewImage.setAttribute('class', 'thumbnail');
+    previewImage.style.backgroundImage = "url('" + getPreviewImage(item) + "')";
+    itemDiv.appendChild(previewImage);
+
+    // Label container on the right
+    var labelContainer = document.createElement("div");
+
+    // Item path
+    var itemPath = document.createElement("h1");
+    itemPath.setAttribute('class', 'path');
+    itemPath.innerHTML = item.path;
+    labelContainer.appendChild(itemPath);
+
+    var actions = [
+      {
+        'icon': 'icon-file',
+        'url': function(item) {
+          let dir = OC.dirname(item.path);
+
+          return OC.generateUrl('/apps/files/?dir=' + dir + '&openfile=' + item.infos.id);
+        },
+        'description': 'Show file'
+      },
+      {
+        'icon': 'icon-details',
+        'url': function(item) {
+          return OC.generateUrl('/f/' + item.infos.id);
+        },
+        'description': 'Show details'
+      }
+    ];
+    var actionsContainer = document.createElement("div");
+    actionsContainer.setAttribute('class', 'fileactions');
+
+    actions.forEach(action => {
+      var action_link = document.createElement('a');
+      action_link.setAttribute('href', action.url.call(null, item));
+      action_link.setAttribute('class', 'action permanent');
+      action_link.setAttribute('title', action.description)
+
+      var action_icon = document.createElement('span');
+      action_icon.setAttribute('class', 'icon ' + action.icon);
+      action_link.appendChild(action_icon);
+
+      actionsContainer.appendChild(action_link);
+    });
+
+    labelContainer.appendChild(actionsContainer);
+
+    // Item hash
+    var hashContainer = document.createElement("h1");
+    hashContainer.setAttribute('class', 'hash');
+    hashContainer.innerHTML = item.hash;
+    labelContainer.appendChild(hashContainer);
+
+    itemDiv.appendChild(labelContainer);
+
+    return itemDiv;
+  }
+
+  function groupBy(items, fn) {
+    var hashes = [];
+    var groupedItems = {};
+    items.forEach(function(item) {
+      var key = fn(item);
+
+      if (!groupedItems[key]) {
+        groupedItems[key] = [];
+        hashes.push(key);
+      }
+
+      groupedItems[key].push(item);
+    });
+
+    return {
+      hashes: hashes,
+      groupedItems: groupedItems
+    };
+  };
+
+  $.getJSON(baseUrl + '/files')
+    .then(function(result) {
+      items = result;
+      render();
+    });
+})();
