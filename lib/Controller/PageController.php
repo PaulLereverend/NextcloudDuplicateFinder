05076462338527f693ea98146ff8ff0800e823d4
Paul <paulereverend@gmail.com>
2021-02-24 20:02:06 +0100

    Init FS for NC21 support

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -5,6 +5,7 @@ use OCP\IRequest;
 use OCP\AppFramework\Http\TemplateResponse;
 use OCP\AppFramework\Http\DataResponse;
 use OCP\AppFramework\Controller;
+use OC\Files\Filesystem;
 
 class PageController extends Controller {
 	private $userId;
@@ -25,7 +26,63 @@ class PageController extends Controller {
 	 * @NoCSRFRequired
 	 */
 	public function index() {
+		//
 		return new TemplateResponse('duplicatefinder', 'index');  // templates/index.php
 	}
+
+	public function files() {
+        $files = $this->getFilesRecursive();
+        $results = \OCA\Files\Helper::formatFileInfos($files);
+        $sizeArr = array();
+        foreach ($results as $key => $result) {
+			$path = $this->getRelativePath($files[$key]->getPath()). $result['name'];
+            $sizeArr[$path] = $result['size'];
+        }
+        unset($files);
+        unset($results);
+
+        $hashArr = array();
+        foreach(array_intersect($sizeArr, array_diff_assoc($sizeArr, array_unique($sizeArr))) as $filePath=>$size){
+            if($info = Filesystem::getLocalFile($filePath)) {
+                $fileHash = hash_file('md5', $info);
+				if($fileHash){
+					$hashArr[$filePath] = $fileHash;
+				}
+            }
+        }
+
+        $duplicatesHash = array_intersect($hashArr, array_diff_assoc($hashArr, array_unique($hashArr)));
+        unset($hashArr);
+        asort($duplicatesHash);
+
+        $response = array();
+		foreach($duplicatesHash as $filePath=>$fileHash) {
+            $file = array();
+            $file['infos'] = \OCA\Files\Helper::formatFileInfo(FileSystem::getFileInfo($filePath));
+            $file['hash'] = $fileHash;
+            $file['path'] = $filePath;
+            array_push($response, $file);
+        }
+        return $response;
+    }
+
+    private function getFilesRecursive(& $results = [], $path = '') {
+		\OC_Util::tearDownFS();
+		\OC_Util::setupFS($this->userId);
+        $files = FileSystem::getView()->getDirectoryContent($path);
+        foreach($files as $file) {
+            if ($file->getType() === 'dir') {
+                $this->getFilesRecursive($results, $path . '/' . $file->getName());
+            } else {
+                $results[] = $file;
+            }
+        }
+        return $results;
+    }
+
+    private function getRelativePath($path) {
+        $path = Filesystem::getView()->getRelativePath($path);
+        return substr($path, 0, strlen($path) - strlen(basename($path)));
+    }
 
 }
