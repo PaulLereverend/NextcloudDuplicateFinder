e1688872b41b79027ebb5cd5811f4ed9da49873a
Christian Rose <christian.rose@chrose.de>
2021-05-04 05:54:30 +0200

    Removed Output redirection

--- tests/bats/helper.sh
+++ tests/bats/helper.sh
@@ -5,13 +5,11 @@ evaluateHashResult(){
   realHash="$(echo "$3" | sha256sum | awk '{ print $1 }')"
   if [[ $(echo "$3" | grep "file_hash" | wc -l ) -ne $2
       || "${realHash}" != "${expectedHash}" ]]; then
-{
     echo "Output is other than expected:"
     echo "Count: $(echo "$3" | grep "file_hash" | wc -l )/$2"
     echo "Exepected: ${expectedHash}"
     echo "Result:    ${realHash}"
     echo "$3"
-} > ${BATS_TEST_NUMBER}_${BATS_TEST_NAME}.out
     return 1
   fi
 }
