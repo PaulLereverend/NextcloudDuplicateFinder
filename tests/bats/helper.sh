aec9e26f87ae9d7ea76a0c4101f4c935a791413c
Christian Rose <christian.rose@chrose.de>
2021-05-06 16:23:10 +0200

    The ubuntu VMs of GHA doesn't have enough entropy see actions/virtual-environments#672

--- tests/bats/helper.sh
+++ tests/bats/helper.sh
@@ -15,5 +15,5 @@ evaluateHashResult(){
 }
 
 randomString() {
-    cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1
+    haveged -n 0  2>/dev/null | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1
 }
